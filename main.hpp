737e2f6bd5f694e5292c5304895e7194088a173f
ogonzalez0018 <156852162+ogonzalez0018@users.noreply.github.com>
2024-02-20 18:49:30 +0000

    1111

--- main.hpp
+++ main.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cstring>
 #include <sstream>
+#include <vector>
 using namespace std;
 struct Name
 {
@@ -33,6 +34,61 @@ void sortPerson(vector<Person> &);
 
 void makePerson(vector<Person> &person)
 {
+  ifstream ifs;
+  string line, buf;
+  stringstream ss;
+  Person p;
+  int fcnt = 0;
+  ifs.open("people-100.csv.10");
+  if (!ifs) {
+    cerr << "Error: file open error" << endl;
+    exit(0);
+  }
+  while (getline(ifs, line)) {
+    ss << line;
+    fcnt = 0;
+    while (getline(ss, buf, ',')) {
+      switch(fcnt)
+        {
+          case 0:
+            p.no = stoi(buf);
+            break;
+          case 1:
+            p.ID = buf;
+            break;
+          case 2:
+            p.name.first = buf;
+            break;
+          case 3:
+            p.name.last = buf;
+            break;
+          case 4:
+            p.gender = buf;
+            break;
+          case 5:
+            p.email = buf;
+            break;
+          case 6:
+            p.phone = buf;
+            break;
+          case 7:
+            p.bdate.year = stoi(buf.substr(0, 4));
+            p.bdate.month = stoi(buf.substr(5, 2));
+            p.bdate.day = stoi(buf.substr(8, 2));
+            break;
+          case 8:
+            p.jobtitle = buf;
+            ss.str("");
+            ss.clear();
+            break;
+          default:
+            cerr << "Field count error" << endl;
+        }
+        fcnt++;
+    }
+    person.push_back(p);
+    ss.clear();
+  }
 }
 void printPerson(vector<Person> person)
 {
@@ -54,7 +110,17 @@ void printPerson(vector<Person> person)
 }
 void sortPerson(vector<Person> &person)
 {
-}
+  // Sort by Last name 
+  for(int i=0;i<person.size()-1;i++)
+    {
+      for(int j=0;j<person.size()-i-1;j++)
+        {
+          if(person[j].name.last>person[j+1].name.last)
+            swap(person[j],person[j+1]);
+    
+        }
+    }
 /*******************************
  * Code your program here
- *******************************/
+*******************************/
+}
